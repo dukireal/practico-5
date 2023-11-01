1bc10d9725438441cfe5cca725864fbe254822e2
dukireal <130255243+dukireal@users.noreply.github.com>
2023-11-01 17:13:04 -0300

    actualizacion factory method

--- Practico5_ejercicio6/main.py
+++ Practico5_ejercicio6/main.py
@@ -2,77 +2,73 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 
 
-class Creador (ABC):
+class Creator(ABC):
+
+# -> anotation, permite indicar que es lo que se devuelve, pero no obliga.
     @abstractmethod
-    def factory_method():
+    def factory_method(self):
         pass
-    
-    def comprar(self):
-        carrito=self.factory_method()
-        resultado=f"nose {carrito.getPrecio()}"
-        return resultado
+
+    def get_precio(self) -> str:
+        product = self.factory_method()
+        result = f"precio total:{product.calcular_precio()}"
+
+        return result
 
 
-class CreadorJuegosDigitales(Creador):
-    def factory_method(self) -> Juegos:
-        return CreadorJuegosDigitales()
+class ConcreteCreator1(Creator):
+    def __init__(self,id_juego,precio,agg) -> None:
+            self._id_juego=id_juego
+            self._precio=precio
+            self._agg=agg
+    def factory_method(self) -> Product:
+        return ConcreteProductJuegoFisico(self._id_juego,self._precio,self._agg)
 
-class CreadorJuegosFisicos(Creador):
-    def factory_method(self) -> Juegos:
-        return CreadorJuegosFisicos()
+class ConcreteCreator2(Creator):
+        def __init__(self,id_juego,precio,agg) -> None:
+            self._id_juego=id_juego
+            self._precio=precio
+            self._agg=agg
+        
+        def factory_method(self) -> Product:
+            return ConcreteProductJuegoDigital(self._id_juego,self._precio,self._agg)
 
 
-class Juegos(ABC):
-    def __init__(self,id_juego,precio):
+class Product(ABC):
+    def __init__(self,id_juego,precio) -> None:
         self._id_juego=id_juego
         self._precio=precio
-    
-    def calcularimporte(self,recargo,precio):
-        precio+=precio*recargo
-        return precio
-    
     @abstractmethod
-    def getPrecio(self) -> float:
+    def calcular_precio(self) -> str:
         pass
 
-class JuegoDigital(Juegos):
-    def __init__(self, id_juego, precio,empresa):
-        super().__init__(id_juego, precio)
-        self._empresa=empresa
-        
-    def getPrecio(self) -> float:
-        match self._empresa:
-            case 1:
-                return self.calcularimporte(0.15,self._precio) 
-            case 2:
-                return self.calcularimporte(0.20,self._precio) 
-            case 3:
-                return self.calcularimporte(0.25,self._precio) 
-
-
-class JuegoFisico(Juegos):
-    def __init__(self, id_juego, precio,delivery):
+class ConcreteProductJuegoFisico(Product):
+    def __init__(self, id_juego, precio,delivery) -> None:
         super().__init__(id_juego, precio)
         self._delivery=delivery
         
-    def getPrecio(self) -> float:
-        match self._delivery:
-            case 1:
-                return self.calcularimporte(0.30,self._precio) 
-            case 2:
-                return self.calcularimporte(0.0,self._precio) 
+    def calcular_precio(self) -> str:
+        self._precio+=self._delivery
+        return self._precio
 
 
-def comprar_juego(creator: Creador):
-    print("usuario generico compra juego generico...")
-    print(f"{creator.comprar()}",end="")
+class ConcreteProductJuegoDigital(Product):
+    def __init__(self, id_juego, precio,comision) -> None:
+        super().__init__(id_juego, precio)
+        self._comicion=comision
+        
+    def calcular_precio(self) -> str:
+        self._precio=self._precio+(self._precio*self._comicion)
+        return self._precio
 
 
-print("comprar por juego fisico")
-id_juego=int(input("id del juego:"))
-precio=float(input("ingrese precio del juego"))
-delivery=int(input("inngrese el tipo de retiro: \n_1-delivery(+ %30) \n2-retiro en sucursal(sin coste adicional)"))
-comprar_juego(CreadorJuegosFisicos())
-print("comprar juego digital:")
-empresa_juego=int(input("ingrse la plataforma difital para comprar el juego: \n1: steam (+%15)\n2:origin(+%20)\n3:epicgames(+%30)"))
-comprar_juego(CreadorJuegosDigitales())
+def client_code(creator: Creator) :
+        print(creator.get_precio())
+
+
+print("precio juego fisico")
+client_code(ConcreteCreator1(1,1200,200))
+print("\n")
+
+print("precio juego digital")
+client_code(ConcreteCreator2(1,1200,0.25))
